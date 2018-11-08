447a545abd17c0a646557855124268da4db716c8
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2018-11-08 08:04:32 +0100

    New widget with service and listview

--- app/src/main/java/me/kaesaecracker/campusDual/Statics.kt
+++ app/src/main/java/me/kaesaecracker/campusDual/Statics.kt
@@ -5,6 +5,15 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.content.Context
+import androidx.core.view.accessibility.AccessibilityEventCompat.setAction
+import android.content.Intent
+
+
+val AppTimeZone: DateTimeZone = DateTimeZone.forOffsetHours(+1)
 
 fun DateTime.getUnixTimestamp() = this.millis / 1000
 
@@ -30,3 +39,26 @@ fun scheduleToString(schedule: List<Schoolday>): String? = gson.myJson<List<Scho
 
 fun stringToSchedule(str: String): List<Schoolday>? = gson.fromJson<List<Schoolday>>(str)
 fun stringToDay(str: String): Schoolday? = gson.fromJson<Schoolday>(str)
+
+
+fun List<Schoolday>?.getCurrentDay(): Schoolday? {
+    if (this == null) return null
+
+    val today = DateTime(AppTimeZone)
+    for (day in this) {
+        if (day.date!!.dayOfYear == today.dayOfYear || day.date!!.isAfterNow)
+            return day
+    }
+
+    return null
+}
+
+fun List<Lesson>.getNonPassedLessons(): List<Lesson> {
+    val list = mutableListOf<Lesson>()
+
+    for (lesson in this) {
+        if (lesson.end.isAfterNow) list.add(lesson)
+    }
+
+    return list
+}
